323834fa0cdfd264bf1a24fd3f1a9c06adf4fd83
Ifeanyichukwu Otiwa <IFEANYICHUKWU OTIWA>
2022-03-13 16:28:45 +0100

    creates dummy test

---
+++ helloworldservice/src/test/java/org/betpawa/helloworldservice/HelloworldserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.betpawa.helloworldservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
